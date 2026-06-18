7e9ce25cfb83b6dd14fc88eea6feb8a67b741e47
Abhinash <abhinashjangapelly@gmail.com>
2026-06-18 12:04:49 +0530

    Fix case-sensitive import bug causing Render deploy failure

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const User = require('../models/user.model');
+const User = require('../models/User.model');
 const AppError = require('../utils/AppError');
 const asyncHandler = require('../utils/asyncHandler');
 
